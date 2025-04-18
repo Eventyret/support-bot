e823beabcafeec1c012f0f1d9562d2ad08b96039
Simen Daehlin <simen.dehlin@gmail.com>
2025-04-18 10:36:34 +0100

    Fixed linting errors for frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,25 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ignores: ["dist/**", "node_modules/**"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ignores: ["dist/**", "node_modules/**"],
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
 ]);
--- frontend/src/components/ui/message-loading.jsx
+++ frontend/src/components/ui/message-loading.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function MessageLoading() {
     return (
         <svg
